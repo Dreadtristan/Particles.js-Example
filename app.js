a74e7a90d4031e5ebdfde462e612b324054f444c
Dreadtristan <94937729+Dreadtristan@users.noreply.github.com>
2022-02-18 18:30:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ particlesJS('particles-js',
       }
     },
     "line_linked": {
-      "enable": false,
+      "enable": true,
       "distance": 200,
       "color": "#6976d7",
       "opacity": 1,
